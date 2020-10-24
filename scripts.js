b8633ae151d655670b37054b0d247042081b0ae4
Teriq Strachan <teriqstrachan@yahoo.com>
2020-10-23 19:36:07 -0500

    Slight html/css changes. Javascript dynamic form functionality added

--- scripts.js
+++ scripts.js
@@ -1,1 +1,24 @@
 /* Add your JavaScript to this file */
+window.onload = function(){
+  let button = Array.from(document.getElementsByClassName("btn"))[1];
+  let p = document.getElementById("news-p");
+  let form = document.getElementById("email-form");
+
+  button.addEventListener("click", event=>{
+    event.preventDefault();
+    let val = document.getElementById("email");
+    console.log(val.value);
+    if(val.value==""){
+      val.placeholder="Enter valid email address";
+    } else{
+      let nval = val.value.italics().fontcolor("#8fe5eb");
+      p.remove();
+      form.remove();
+      let dev = Array.from(document.getElementsByClassName("message"))[0];
+      let h2 = document.createElement("h2");
+      h2.innerHTML="Thank You! Your email address "
+      +nval+" has been added to our mailing list!";
+      dev.appendChild(h2);
+    }
+  })
+};
